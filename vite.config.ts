99a92482cdcfbf488f4db3240f5e3410100ca0c2
Chia <chia.chen@unionfly.tech>
2022-08-21 00:44:00 +0800

    [ADD]Element plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      deep: true,
+      dts: false,
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
         chunkFileNames: 'static/js/[name]-[hash].js',
         entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        manualChunks(id) {
+          if (id.includes('static/css/index.css')) {
+            return 'tailwindcss'
+          }
+          if (id.includes('element-plus/theme-chalk/')) {
+            return 'element-plus'
+          }
+        }
       }
     }
   },
